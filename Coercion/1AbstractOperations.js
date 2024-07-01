d02f3868e2fdf4b3c37db3242aa91c1ca547d7d4
sandeep <sandeepmukherjee12@gmail.com>
2024-07-01 18:36:17 +0530

    Abstract Operations

---
+++ Coercion/1AbstractOperations.js
@@ -0,0 +1,7 @@
+// DOCS : https://262.ecma-international.org/#sec-abstract-operations
+
+// Definition 1 : These are some set of algorithms, that is present in the ecmascript docs but they are not available for usage in ecmascript
+//Ex : We developers cannot use these opeartions directly
+
+// Definition 2 : They are mentioned in the docs to (aid)help the documentation only 
+// Ex : In ECMA Docs there are lot of things that are done by the language internally.To explain these internal details of how and what language is doing we have abstract opeartions mentioned in the DOCS 
