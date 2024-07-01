b9f92fbe0aeffffc2df8ab0d632659a8950c903c
sandeep <sandeepmukherjee12@gmail.com>
2024-07-01 22:01:49 +0530

    TheSubtractionOperator

---
+++ Coercion/3TheSubtractionOperator.js
@@ -0,0 +1,21 @@
+/* 
+Docs - https://262.ecma-international.org/10.0/#sec-subtraction-operator-minus
+After Storing the leftoperand/rightoperand value in lref/rref from step 1-4
+Step 5 and 6 convert the values ToNumber() and store in lnum/rnum
+
+Refer Docs - https://262.ecma-international.org/10.0/#sec-tonumber
+
+Ex-1) 10 - null
+10 is a Number so no conversion will be there as mentioned in the DOCS
+null conversion(ToNumber(null)) returns +0 
+
+Output :
+10-0 = 10
+
+Ex-2) 10 - undefined
+10 is converted to Number so it remains 10
+undefined conversion(ToNumber(undefined)) returns NaN 
+Output:
+10 - NaN = NaN
+
+*/
