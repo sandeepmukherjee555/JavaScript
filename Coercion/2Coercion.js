f34ef212ec42e66786ca74a77a629624d769a7d0
sandeep <sandeepmukherjee12@gmail.com>
2024-07-01 22:00:44 +0530

    Coercion

---
+++ Coercion/2Coercion.js
@@ -0,0 +1,18 @@
+/*
+
+Whenever we do an operation(addition,substraction etc) based on the input 
+we can actually CONVERT the input for the operation
+
+CONVERT - we can convert type of input
+
+There are two types of conversion -
+
+Typecasting can be referred to as type conversion
+
+Explicit Typecasting : This conversion can be manually done by us
+
+Inplicity Typecasting/Internally or Automatically(Coercion) : The Language based on some certain rules automatically converts the types
+
+All the Point under 7.1 https://262.ecma-international.org/#sec-abstract-operations like ToPrimitive,ToBoolean  all are abstract operations 
+
+*/
