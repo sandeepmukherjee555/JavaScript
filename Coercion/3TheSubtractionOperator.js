2a2d93bc26385303f945a3e3ed15094d388a157b
sandeep <sandeepmukherjee12@gmail.com>
2024-07-02 21:58:38 +0530

    Ex Added for Sub Operation

--- Coercion/3TheSubtractionOperator.js
+++ Coercion/3TheSubtractionOperator.js
@@ -7,15 +7,21 @@ Refer Docs - https://262.ecma-international.org/10.0/#sec-tonumber
 
 Ex-1) 10 - null
 10 is a Number so no conversion will be there as mentioned in the DOCS
-null conversion(ToNumber(null)) returns +0 
+null conversion ToNumber(null) returns +0 
 
 Output :
 10-0 = 10
 
 Ex-2) 10 - undefined
 10 is converted to Number so it remains 10
-undefined conversion(ToNumber(undefined)) returns NaN 
+undefined conversion ToNumber(undefined) returns NaN 
 Output:
 10 - NaN = NaN
+
+Ex-3) 10 - true
+10 is converted to Number so conversion is 10
+true conversion ToNumber(true) returns 1
+Output:
+10 - 1 = 9
 
 */
