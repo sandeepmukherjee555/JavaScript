c281c5d4dd767ed05c449f662b3640b8661fa054
sandeep <sandeepmukherjee12@gmail.com>
2024-07-20 23:31:47 +0530

    functions

---
+++ Scopes/7functionExpression.js
@@ -0,0 +1,19 @@
+function fun() { //function declaration
+
+}
+
+let f = function gun() {//function expression
+
+}
+
+let a = function () {//function expression
+
+}
+
+    (function x() {//function expression
+
+    })
+
+    (function () {//function expression
+
+    })
