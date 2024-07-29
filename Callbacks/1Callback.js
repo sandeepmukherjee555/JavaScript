c278fd4b7a9fbbf3257021711447a83f88fba9a3
sandeep <sandeepmukherjee12@gmail.com>
2024-07-29 22:50:56 +0530

    callback

---
+++ Callbacks/1Callback.js
@@ -0,0 +1,15 @@
+/*
+fun -> HOF -> it takes fn(which is a function as argument)
+x -> number 
+fn -> function
+*/
+function fun(x, fn) {
+    for (let i = 0; i < x; i++) {
+        console.log(i);
+    }
+    fn();
+}
+fun(10, function exec() {
+    console.log("I am executed also");
+});
+//Here exec is a callback function
