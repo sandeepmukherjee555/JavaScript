64bdcff3cc686ad3f24531862b19adb4a2ed38df
sandeep <sandeepmukherjee12@gmail.com>
2024-08-16 09:48:16 +0530

    Promise Docs

---
+++ Promises/1Docs.js
@@ -0,0 +1,14 @@
+/*
+Docs : https://262.ecma-international.org/10.0/#sec-promise-objects
+Promises are native to JavaScript handling that thing is syncronous in nature
+Creation of a Promise Object is Synchronous in nature
+
+There are 3 states of a Promise Object
+
+1) Pending -> When we create a new Promise Object this is the default state,it represents work in progress
+
+2) filfilled -> if operation is completed successfully
+
+3) rejected -> if operation was not successful then it will go in a rejected state
+
+*/
