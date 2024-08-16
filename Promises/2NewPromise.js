833695f4c391057b6f29d7b623851e31e684b425
sandeep <sandeepmukherjee12@gmail.com>
2024-08-16 10:02:10 +0530

    creating new Promise

---
+++ Promises/2NewPromise.js
@@ -0,0 +1,13 @@
+/*
+
+Constructor is a special function using which new object can be created
+Docs:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+new Promise(f) // this constructor expects a callback f
+
+new Promise(function(resolve,reject){
+// inside function we can write our own time consuming task
+})
+
+*/
+
