da411a2b11a478e0d97295729cb362a73ec368ed
sandeep <sandeepmukherjee12@gmail.com>
2024-08-16 22:25:34 +0530

    Promise Docs updated

--- Promises/2NewPromise.js
+++ Promises/2NewPromise.js
@@ -9,5 +9,16 @@ new Promise(function(resolve,reject){
 // inside function we can write our own time consuming task
 })
 
+Whenever in the implementation of the executor callback you call the resolve function,
+the promise goes to a fulfilled state 
+If you call reject function,it goes to a rejected state and if you don't call anything
+promise remains in pending state 
+
+Apart from state property promise also has value property
+
+Till the time state is Pending value is undefined
+
+Whatever we pass as a argument of resolve the state of the object will be fulfilled state,
+and value will be the argument passed inside resolve 
 */
 
