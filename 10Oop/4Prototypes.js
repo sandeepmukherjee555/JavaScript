2bb9c74e997509b82564ec2839b41126819adb1e
sandeep <sandeepmukherjee12@gmail.com>
2024-09-15 18:39:10 +0530

    Prototypes

---
+++ 10Oop/4Prototypes.js
@@ -0,0 +1,6 @@
+/*
+Objects are created by constructor calls using new keyword
+What is Prototypes?
+Ans : It is a mechanism using which Js objects inherits features from one another
+Every object will be by default having a property called as prototype
+*/
